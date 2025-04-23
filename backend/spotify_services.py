599cc900599f0eaa493fb5afbeeb823a53c192e5
Ella Rushing <ellarushing2022@gmail.com>
2025-04-22 19:44:21 -0700

    playlist and me endpoints work

--- backend/spotify_services.py
+++ backend/spotify_services.py
@@ -1,5 +1,4 @@
 # use this to get the users profile
-
 import requests
 
 
@@ -12,6 +11,18 @@ def get_user_profile(access_token):
     response = requests.get("https://api.spotify.com/v1/me", headers=headers)
     response.raise_for_status() # get an error if request failed
     return response.json() # return json of the request
+
+
+def get_user_playlists(access_token):
+    headers = {
+        # take token and send it to the Spotify endpoint
+        "Authorization": f"Bearer {access_token}"
+    }
+    url = "https://api.spotify.com/v1/me/playlists"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
 
 
 
